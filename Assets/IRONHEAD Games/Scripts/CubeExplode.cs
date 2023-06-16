7e37c63138f43eb0ae94e76f74cb203aa1d717f9
Ajinkyapise123 <ajinkya@anubudh.com>
2023-06-15 20:41:35 -0600

    Commit

---
+++ Assets/IRONHEAD Games/Scripts/CubeExplode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExplode : MonoBehaviour
+{
+
+    public GameObject shatteredObject;
+    public GameObject mainCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCube.SetActive(true);
+        shatteredObject.SetActive(false);
+    }
+
+    public void IsShot()
+    {
+        Destroy(mainCube);
+        shatteredObject.SetActive(true);
+        var shatterAnimation = shatteredObject.GetComponent<Animation>().Play();
+
+        //Vibration of controller
+        OVRInput.SetControllerVibration(1,1,OVRInput.Controller.RTouch);
+        // Add score
+        ScoreManager.instance.AddScore(ScorePoints.GUNCUBE_SCOREPOINT);
+
+        Destroy(shatteredObject,1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            IsShot();
+        }
+    }
+
+
+}
---
+++ Assets/IRONHEAD Games/Scripts/SceneLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public OVROverlay overlay_Background;
+    public OVROverlay overlay_LoadingText;
+
+
+
+
+    public static SceneLoader instance;
+    private void Awake()
+    {
+        if (instance !=null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(ShowOverlayAndLoad(sceneName));
+    }
+
+    IEnumerator ShowOverlayAndLoad(string sceneName)
+    {
+        overlay_Background.enabled = true;
+        overlay_LoadingText.enabled = true;
+
+        GameObject centreEyeAnchor = GameObject.Find("CenterEyeAnchor");
+        overlay_LoadingText.gameObject.transform.position = centreEyeAnchor.transform.position + new Vector3(0f, 0f, 3f);
+
+        //waiting for seconds to pop
+        yield return new WaitForSeconds(3f);
+
+        //Load scene and wait unitll complete
+
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName); 
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // disabling overlays
+        overlay_Background.enabled = false;
+        overlay_LoadingText.enabled = false;
+
+        yield return null;    
+    }
+}
